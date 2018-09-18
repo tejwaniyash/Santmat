4dea2c2d4af0cbef4320e5004c59a50e8e3870c1
Rishabh Singh <pingrishabh@gmail.com>
2018-09-18 16:31:22 +0530

    V 2.0.2 Beta
    - Splash screen doesn't proceed if there is no internet connection
    
    - Added Progress Loader
    
    - Added Refresh Loader

--- app/src/main/java/com/example/itskh/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/itskh/myapplication/SplashActivity.java
@@ -1,16 +1,45 @@
 package com.example.itskh.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String playlistUrl = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCEfqzmMwibbzRK879P9kEPw&key=AIzaSyDEYPFMLPlAhKf3Fw8hPtLE4jcZFgGYXCY&maxResults=50";
+    private List<playlistData> Ytdata;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
-        finish();
+        if (internetIsAvailable()) {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Please check your internet connection!", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    public boolean internetIsAvailable() {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
 }
